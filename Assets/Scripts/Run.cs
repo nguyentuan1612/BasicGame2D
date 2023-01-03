01e2c7d6e19cfd928ee05fb96864c65b9f76dc26
Nguyen Tuan <tuanbeo203@gmail.com>
2023-01-03 23:12:50 +0700

    Run.cs

---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+    public Animator ani; // nhiệm vụ quản lí toàn bộ animation
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // lấy toàn bộ animation của thằng animator của thằng unity
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ani.SetBool("isRunning", true);
+            ani.Play("Running");
+            transform.Translate(Time.deltaTime * 3,0,0);
+            transform.localScale = new Vector3(1F,1F,1F );
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            ani.SetBool("isRunning", true);
+            ani.Play("Running");
+            transform.Translate(Time.deltaTime * -3,0,0);
+            transform.localScale = new Vector3(-1F,1F,1F );
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0,Time.deltaTime * 15,0);
+        }
+        else
+        {
+            ani.SetBool("isRunning",false);
+        }
+        
+    }
+}
