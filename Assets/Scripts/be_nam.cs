ee4da593d759ac4f43aef1dc4c174e256b9417b4
Nguyen Tuan <tuanbeo203@gmail.com>
2023-01-04 11:56:18 +0700

    be_nam.cs

---
+++ Assets/Scripts/be_nam.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class be_nam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-Time.deltaTime,0,0);
+        
+        
+    }
+}
