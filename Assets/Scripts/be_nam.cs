5ab084e09685f6138d04f399db9930af11c40244
Nguyen Tuan <tuanbeo203@gmail.com>
2023-01-05 22:36:22 +0700

    be_nam.cs

--- Assets/Scripts/be_nam.cs
+++ Assets/Scripts/be_nam.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class be_nam : MonoBehaviour
 {
+    public bool gioiHan = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,21 @@ public class be_nam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-Time.deltaTime,0,0);
-        
-        
+        if (gioiHan == true)
+        {
+            transform.Translate(-Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.Translate(Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "gioi_han")
+        {
+            gioiHan = false;
+        }
     }
 }
